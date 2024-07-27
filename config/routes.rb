65b157bf18ae1c6dbfad9b24267d91b3a5e43176
YOUR NAME <YOUR@EMAIL.com>
2024-07-27 13:26:37 -0600

    update to pull chat data from our api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'call_rail_data/fetch_and_store', to: 'call_rail_data#fetch_and_store'
       get 'call_rail_data', to: 'call_rail_data#index'
+      get 'gravity_forms_data', to: 'gravity_forms#index'
+      post 'gravity_forms/fetch_and_save/:company_name', to: 'gravity_forms#fetch_and_save'
+      get 'gravity_forms/entries', to: 'gravity_forms#entries'
     end
   end
 end
