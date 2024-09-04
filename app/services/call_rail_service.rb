7381541392d2a8284951f9a4e3a41c5f3a82f7d2
YOUR NAME <YOUR@EMAIL.com>
2024-09-04 10:23:07 -0500

    update api key.

--- app/services/call_rail_service.rb
+++ app/services/call_rail_service.rb
@@ -2,7 +2,7 @@ class CallRailService
   include HTTParty
   base_uri 'https://api.callrail.com/v3/a'
 
-  def initialize(api_key = '53962143e3bd0ab2989770ecbe94a75c', company_id = nil)
+  def initialize(api_key = ENV['CALLRAIL_API_KEY'], company_id = nil)
     @api_key = api_key
     @company_id = company_id
   end
