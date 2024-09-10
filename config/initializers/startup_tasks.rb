3e9ac6d4482d8fa8470a8fe379ac2801fa4d8f5b
YOUR NAME <YOUR@EMAIL.com>
2024-09-10 17:10:23 -0500

    update rakes

--- config/initializers/startup_tasks.rb
+++ config/initializers/startup_tasks.rb
@@ -3,9 +3,15 @@ Rails.application.config.after_initialize do
   Rails.application.load_tasks
 
   begin
+    Rails.logger.info "Invoking Rake task: call_rail:fetch_and_save_all"
     Rake::Task['call_rail:fetch_and_save_all'].invoke
+    Rails.logger.info "Successfully invoked Rake task: call_rail:fetch_and_save_all"
+    
+    Rails.logger.info "Invoking Rake task: gravity_forms:fetch_and_save"
     Rake::Task['gravity_forms:fetch_and_save'].invoke
+    Rails.logger.info "Successfully invoked Rake task: gravity_forms:fetch_and_save"
   rescue => e
     Rails.logger.error "Error running startup tasks: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
   end
 end
