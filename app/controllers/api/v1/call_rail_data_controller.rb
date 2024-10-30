dfae8b9cf57722327526f925832081a512cd5bd5
YOUR NAME <YOUR@EMAIL.com>
2024-10-30 09:39:07 -0600

    recreate call rail data controller

--- app/controllers/api/v1/call_rail_data_controller.rb
+++ app/controllers/api/v1/call_rail_data_controller.rb
@@ -1,50 +1,57 @@
-namespace :call_rail do
-  desc "Fetch and save call data for all companies"
-  task fetch_and_save_all: :environment do
-    COMPANY_MAPPING.each do |company_id, company_name|
-      Rails.logger.info "Processing company: #{company_name} with ID: #{company_id}"
+module Api
+  module V1
+    class CallRailDataController < ApplicationController
+      def index
+        if params[:company_id]
+          Rails.logger.info "Fetching data for Company ID: #{params[:company_id]}"
+          service = params[:company_id] == '176449112' ? GgtCallRailService.new : CallRailService.new(ENV['CALLRAIL_API_KEY_PRIMARY'], ENV['CALLRAIL_COMPANY_ID_PRIMARY'], params[:company_id])
+          response = service.fetch_calls
 
-      # Use GgtCallRailService for GGT company ID
-      service = company_id == '176449112' ? GgtCallRailService.new : CallRailService.new(ENV['CALLRAIL_API_KEY_PRIMARY'], ENV['CALLRAIL_COMPANY_ID_PRIMARY'], company_id)
-
-      # Fetch data
-      response = service.fetch_calls
-
-      if response.success?
-        Rails.logger.info "Successful response for #{company_name}"
-
-        # Process and save each call
-        response.parsed_response["calls"].each do |call|
-          Rails.logger.info "Processing call with ID: #{call['id']} for Company ID: #{company_id}"
-          call_rail_data = CallRailData.find_or_initialize_by(call_id: call["id"])
-
-          call_rail_data.assign_attributes(
-            answered: call["answered"],
-            business_phone_number: call["business_phone_number"],
-            customer_city: call["customer_city"],
-            customer_country: call["customer_country"],
-            customer_name: call["customer_name"],
-            customer_phone_number: call["customer_phone_number"],
-            customer_state: call["customer_state"],
-            direction: call["direction"],
-            duration: call["duration"],
-            recording: call["recording"],
-            recording_duration: call["recording_duration"],
-            recording_player: call["recording_player"],
-            start_time: call["start_time"],
-            tracking_phone_number: call["tracking_phone_number"],
-            voicemail: call["voicemail"],
-            company_id: company_id
-          )
-
-          if call_rail_data.save
-            Rails.logger.info "Call with ID: #{call['id']} saved successfully for Company ID: #{company_id}"
+          if response.success?
+            Rails.logger.info "Successful response for Company ID #{params[:company_id]}"
+            response.parsed_response["calls"].each do |call|
+              unless CallRailData.exists?(call_id: call["id"])
+                begin
+                  CallRailData.create!(
+                    answered: call["answered"],
+                    business_phone_number: call["business_phone_number"],
+                    customer_city: call["customer_city"],
+                    customer_country: call["customer_country"],
+                    customer_name: call["customer_name"],
+                    customer_phone_number: call["customer_phone_number"],
+                    customer_state: call["customer_state"],
+                    direction: call["direction"],
+                    duration: call["duration"],
+                    call_id: call["id"],
+                    recording: call["recording"],
+                    recording_duration: call["recording_duration"],
+                    recording_player: call["recording_player"],
+                    start_time: call["start_time"],
+                    tracking_phone_number: call["tracking_phone_number"],
+                    voicemail: call["voicemail"],
+                    company_id: params[:company_id]
+                  )
+                  Rails.logger.info "Call with ID: #{call['id']} saved successfully"
+                rescue => e
+                  Rails.logger.error "Failed to save call with ID: #{call['id']} - Error: #{e.message}"
+                end
+              else
+                Rails.logger.info "Skipping duplicate call with ID: #{call['id']}"
+              end
+            end
           else
-            Rails.logger.error "Error saving call with ID: #{call['id']} for Company ID: #{company_id}: #{call_rail_data.errors.full_messages.join(', ')}"
+            Rails.logger.error "Error response from service: #{response.message}"
+            calls = CallRailData.none
           end
+        else
+          calls = CallRailData.all
         end
-      else
-        Rails.logger.error "Error response for #{company_name}: #{response.message}"
+
+        enriched_calls = calls.map do |call|
+          call.attributes.merge(company_name: COMPANY_MAPPING[call.company_id])
+        end
+
+        render json: enriched_calls
       end
     end
   end
